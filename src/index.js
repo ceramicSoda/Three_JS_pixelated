be243620707dd7cc722996c6b93ee38517e0a84b
space8ar <Dmch.d89@gmail.com>
2021-12-25 13:09:12 +0200

    UV fixed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import {Scene,
         Mesh, 
         OrthographicCamera, 
         MeshPhysicalMaterial,
-        IcosahedronGeometry,
+        BoxGeometry,
         Color,
         SpotLight,
         AmbientLight,
-        TextureLoader} from 'three'
+        TextureLoader,
+        NearestFilter} from 'three'
 
 function radToDeg(angle){return((angle*180)/Math.PI)}
 function degToRad(angle){return((angle/180)*Math.PI)}
@@ -19,31 +20,44 @@ class World{
     this._viewport = {height: this.canvas.clientWidth / 2, 
                       width: this.canvas.clientHeight / 2,
                       aspect: this.canvas.clientWidth/this.canvas.clientHeight,
-                      zoom: 2
+                      zoom: 1
                     };
     this.time = 0; 
     this.init();
     this.render();
+    //this.initEventHandlers();
   }
   
   init(){
+    this.initScene();
+    this.renderer = new WebGLRenderer({
+      antialias: false,
+      canvas: this.canvas
+    });
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight)
+    this.renderer.setPixelRatio(window.devicePixelRatio/8);
+  }
+
+  initScene(){
     const isoAngle = 1.22474487139; 
     this.texture = new TextureLoader().load("src/checker.png");
-    this.angleX = 0;
+    this.texture.minFilter = NearestFilter; 
+    this.texture.magFilter = NearestFilter; 
     this.scene = new Scene();
     this.scene.background = new Color( 0x151515 );
-    this.geometry = new IcosahedronGeometry(1,1);
+    //this.geometry = new IcosahedronGeometry(1,1);
+    this.geometry = new BoxGeometry();
     this.material = new MeshPhysicalMaterial({
       map: this.texture,
       color: 0x888888,
       flatShading: true,
-      roughness: 0.2,
+      roughness: 0.3,
       reflectivity: 1,
     });
     this.mesh = new Mesh(this.geometry, this.material)
-    this.spotlight1 = new SpotLight(0xff8f47);
-    this.spotlight2 = new SpotLight(0x7ec4f1);
-    this.ambientlight1 = new AmbientLight(0x303030);
+    this.spotlight1 = new SpotLight(0xff8f47, 3);
+    this.spotlight2 = new SpotLight(0x7ec4f1, 3);
+    this.ambientlight1 = new AmbientLight(0x455045);
     this.camera = new OrthographicCamera( this._viewport.aspect * -this._viewport.zoom, 
                                           this._viewport.aspect * this._viewport.zoom, 
                                           this._viewport.zoom, 
@@ -54,12 +68,14 @@ class World{
     this.camera.position.z = isoAngle*5
     this.camera.lookAt(this.mesh.position);
     this.scene.add(this.camera, this.mesh, this.spotlight1, this.spotlight2, this.spotlight3, this.ambientlight1);
-    this.renderer = new WebGLRenderer({
-      antialias: false,
-      canvas: this.canvas
-    });
-    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight)
-    this.renderer.setPixelRatio(window.devicePixelRatio/4);
+  }
+
+  initEventHandlers(){
+    window.addEventListener('resize', () => {
+      this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight)
+      this.camera.updateProjectionMatrix();
+      
+    })
   }
 
   render(){
